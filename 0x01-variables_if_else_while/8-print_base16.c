b6576c39d733977f74e1fef684b4ee32a6c19156
Khalid <Khalid.mayowa@gmail.com>
2023-02-20 17:19:22 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the number of base 16 in lowercase,
+ * followed by a new line,
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+	int a_to_f = 'a';
+
+	while (n <= '9') /*print 0-9*/
+	{
+		putchar(n);
+		n++;
+	}
+
+	while (a_to_f <= 'f') /*print a-f to finish hexbase*/
+	{
+		putchar(a_to_f);
+		a_to_f++;
+	}
+
+	putchar('\n');
+	return (0);
+}
