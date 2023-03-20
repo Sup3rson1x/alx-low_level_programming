290fe41782e0d49fe2be57e296648d29d43bd58f
Khalid <Khalid.mayowa@gmail.com>
2023-03-20 10:27:10 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
-
 #ifndef DOG
 #define DOG
+
 /**
  * struct dog - is a structure for a dog object
  * @name: is the name element
  * @age: is the age element
  * @owner: is the owner element
  */
+
 struct dog
 {
 	char *name;
